95669759d68b28ebd07657729e507fc8169f4b9b
Evening <evening@admins-iMac.local>
2022-11-28 20:13:10 +0800

    js

---
+++ .vscode/js lesson/js-basic/app.js
@@ -0,0 +1,167 @@
+
+// "Comments can make code readable"
+// "Java script lesson begin"
+/*"Comments can make code readable"*/
+let ner = "Muugii";
+let nas = 37;
+let child = true;
+let job= "engineer";
+
+console.log(ner);
+console.log(nas);
+console.log(child);
+console.log(job);
+console.log(typeof(nas));
+// utga onooh, ugta onoogoogui 6,7
+let a;
+let x=10;
+console.log(a);
+console.log(x);
+// neg murund iim nertei huvsagch zarlah 9-r dasgal
+let firstname="Naraa" , lastname="Saraa", maritalstatus="huviin", country="mongolia", age="25"; 
+
+console.log("firstname"+" "+ "lastname"+" "+ "maritalstatus"+" "+ "country"+" " +"age");
+// olon murund iim neruudtei hevisagch zarlah
+let firstname1="Saraa";
+let lastname2="Naraa";
+let maritalstatus1="niitiin";
+let country1="korea";
+let age1="20";
+// 10-r dasgal
+let myage=20; 
+let yourage=myage;
+console.log(yourage);
+// 10-r dasgal
+// 11-r dasgal tegsh untsugt gurvaljingiin talbai oloh
+let c=5;
+let h=8;
+let s=c*h/2;
+console.log(c);
+console.log(h);
+console.log("c+h=" + s);
+// 11-r dasgal duusav
+// 12-r dasgal
+const PTI=3.14;
+let r=5;
+let talbai=PTI*r**2;
+console.log(talbai);
+// 12-r dasgal duusav
+// 13-r dasgal
+let y2=50;
+let y1=10;
+let x2=8;
+let x1=3;
+let m=(y2-y1)/x2-x1;
+console.log(m);   /* 13-r dasgal duusav*/
+
+let g=8000000000; /* 1gega */
+let byte=15;
+let sum=800000000*15;
+console.log(sum); /* 14-r dasgal duusav */
+
+let time=3;     /*15-r dasgal */
+let mega=15*1000000;
+let bitrate=(15*1000000)/3;
+console.log(bitrate);  /* 15-r dasgal */
+ 
+let side1=5;    /* 16-r dasgal ehlev */
+let side2=8;
+let side3=13;
+let sidesum=(side1+side2+side3)/2;
+console.log(sidesum);   /*16-r dasgal duusav */
+
+let tseils=20;
+let parengit= tseils*1.8+32;
+console.log(parengit);  /* 17-r bodlogo */
+
+/* 18-r bodlogo */
+let parenget =30;
+let tseil=1.8*(30-32);
+console.log(tseil); /* 18-r bodlogo */
+
+let num1=5;    /*19-r boglogo */
+let num2=20;
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2); 
+console.log(num1%num2);         /*19-r bodlogo duusav */
+
+const inch=2.54;    /*20-r ehlev */
+let sm=4;
+console.log(inch*4);  /*20-r dasgal duusav */
+
+// N21
+let radius=2;
+const PI=3.14;
+let v=4/3*PI*radius**3;
+let d=radius*2;
+let huree=PI*d;
+console.log("v=",v); 
+console.log("huree=", huree);
+// N22
+let f=356;
+
+let j=356%100;
+let k=f-j;
+let et=k/100;
+console.log("e=",et);
+
+// N23
+let h1,h2,h3,h4,h5,h6,h7,h8,h9,h10;
+h1="Baby Shark";
+h2=",doo-doo";
+h3="Mommy Shark";
+h4="Daddy Shark";
+h5="Grandma Shark";
+h6="Grandpa Shark";
+h7="Let's go hunt";
+h8="Run away";
+h9="Safe at lastname";
+h10="It's the end";
+console.log(h1+" "+h2+" "+h2); 
+console.log(h1+" "+h2+" "+h2); 
+console.log(h1+" "+h2+" "+h2); 
+console.log(h1);
+
+console.log(h3+ " "+h2+" "+h2);
+console.log(h3+ " "+h2+" "+h2);
+console.log(h3+ " "+h2+" "+h2);
+console.log(h3);
+
+console.log(h4+" "+h2+" "+h2);
+console.log(h4+" "+h2+" "+h2);
+console.log(h4+" "+h2+" "+h2);
+console.log(h4);
+
+console.log(h5+" "+h2+" "+h2);
+console.log(h5+" "+h2+" "+h2);
+console.log(h5+" "+h2+" "+h2);
+console.log(h5);
+
+console.log(h6+" "+h2+" "+h2);
+console.log(h6+" "+h2+" "+h2);
+console.log(h6+" "+h2+" "+h2);
+console.log(h6);
+
+console.log(h7+" "+h2+" "+h2);
+console.log(h7+" "+h2+" "+h2);
+console.log(h7+" "+h2+" "+h2);
+console.log(h7);
+
+console.log(h8+" "+h2+" "+h2);
+console.log(h8+" "+h2+" "+h2);
+console.log(h8+" "+h2+" "+h2);
+console.log(h8);
+
+console.log(h9+" "+h2+" "+h2);
+console.log(h9+" "+h2+" "+h2);
+console.log(h9+" "+h2+" "+h2);
+console.log(h9);
+
+console.log(h10+" "+h2+" "+h2);
+console.log(h10+" "+h2+" "+h2);
+console.log(h10+" "+h2+" "+h2);
+console.log(h10);
+
+
