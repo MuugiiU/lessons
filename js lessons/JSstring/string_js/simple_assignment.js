a7a97505b40582a41daa77cd6365ffc83105b034
Evening <evening@admins-iMac.local>
2022-11-30 21:45:03 +0800

    g

---
+++ js lessons/JSstring/string_js/simple_assignment.js
@@ -0,0 +1,29 @@
+// n-1
+let name='Munkhtsetseg';
+console.log(name.length);
+let lastname='Urtnasan';
+console.log(lastname.length);
+console.log("========n-1 finish====");
+// n-2
+ let str="Q W E R T Y U I O P A S D F G H J K L Z X C V B N M";
+ console.log(str.toLowerCase());
+ console.log("========n-2=======");
+//  n-3
+ console.log(str.toUpperCase());
+ console.log('==========n-3=======');
+//   n-4
+let pine='Pinecone academy-н';
+let leap=pine.concat(' ','leap хөтөлбөрт тавтай морилно уу');
+console.log(leap);
+console.log("=======n-4===========");
+// n-5
+let lastName="Уртнасан овогтой";
+let firstName="Мөнхцэцэг ";
+let country="Улаанбаатар хотод амьдардаг";
+let contact=lastName.concat()+firstName.concat()+country.concat();
+ console.log(contact);
+ console.log("===========n-5============");
+//  n-6
+
+
+
