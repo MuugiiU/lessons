ae5be9425bc3d5e7bc9736301d6dd59c228f7b7d
Evening <evening@admins-iMac.local>
2022-12-01 19:15:09 +0800

    m

---
+++ js lessons/JS Loop/loopjs/loop.js
@@ -0,0 +1,74 @@
+// let count = 1;
+// while (count <= 10) {
+//     console.log(count);
+//     count = count + 1;
+// }
+// console.log("==============n-1===========");
+// //  n-2 
+// let count1 = 0;
+// while (count1 <= 10) {
+//     if (count1 % 2 !== 0) {
+//         console.log(count1);
+//     }
+//     count1 += 1;
+// }
+// console.log("===========n-2=============");
+// // n-3
+// let c = 0;
+// while (c <= 10) {
+//     if (c % 2 === 0) {
+//         console.log(c);
+//     }
+//     c = c + 1;
+// }
+// console.log("=============n-3==========");
+// // n-4 
+// let num=1;
+// let sum=0;
+// while(num<=100) {
+//     sum=sum+num;
+//     num +=1;
+    
+//     }
+//    console.log(`100 хүртлэх тооны нийлбэр:${sum}`);
+//    console.log("=============n-4===========");
+  
+   // n-5
+   
+   let n=parseInt(prompt("Та тоо оруулна уу"));
+   let nem=1;
+   let sum1=0;
+
+    while (nem<=n) {
+        sum1=sum1+nem
+        nem+=1;
+    }
+        console.log(`n хүртлэх тооны нийлбэр: ${sum1}`);
+      
+   
+
+
+//  n-6
+// let a=parseInt(prompt("дурын тоогоо оруулна уу"));
+// let isPrime = true;
+// if (a === 2) {
+//     console.log(`${a} бол анхны тоо мөн.`)
+// } else {
+//     let i = 2;
+//     while (i <a) {
+//         if(a% i === 0) {
+//             isPrime = false;
+//             break;
+//         } 
+//         i++;
+//     } 
+//     if (isPrime) {
+//             console.log(`${a}("Анхны тоо мөн")`);
+        
+//         } else {
+//             console.log(`${a} бол анхны тоо биш мөн.`)
+//         }
+       
+//     }
+ 
+
