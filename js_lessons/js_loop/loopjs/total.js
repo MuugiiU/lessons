b64163920ccafa521b7b40597c2d58b348399e95
Evening <evening@admins-iMac.local>
2022-12-02 17:57:33 +0800

    d

---
+++ js_lessons/js_loop/loopjs/total.js
@@ -0,0 +1,34 @@
+// // Urbuu too
+// let num = parseInt(prompt("Та тоогоо оруулна уу"));
+// let revNum = 0;
+// let unit =0;
+// console.log("Таны оруулсан тоо:", num);
+// while (num !==0 ) {
+//   unit = num % 10;
+//   revNum = revNum * 10 + unit ;
+//   num = Math.floor(num / 10) ;
+// }
+
+// console.log("Таны оруулсан тооны урвуу тоо ", revNum);
+
+// console.log("==============n-1================");
+//  n-2
+// let n=parseInt(prompt("Та тоо оруулна уу"));
+// let nem=1;
+// let sum1=0;
+
+//  while (nem<=n) {
+//      sum1=sum1+nem
+//      nem+=1;
+//  }
+//      console.log(`гараас оруулсан  хүртлэх тооны нийлбэр: ${sum1}`);
+
+ // n-3
+let c = 0;
+while (c <= 100) {
+    if (c % 2 === 0) {
+        console.log(c);
+    }
+    c = c + 1;
+}
+console.log("=============n-3==========");
