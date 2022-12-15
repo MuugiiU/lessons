02cc51c162e16baeabca29d07d8d8fbd9e4231d4
Evening <evening@admins-iMac.local>
2022-12-15 18:47:42 +0800

    n

--- js_lessons/DOM/js/js/jishee.js
+++ js_lessons/DOM/js/js/jishee.js
@@ -11,20 +11,23 @@ const button=document.getElementById('btn');
     // эдгээр нөхцөл биелсэн бол true буцаана
   
 
-let medeelel = () => {
-    console.log(email.value);
-    console.log(password.value);
-    console.log(repeat.value);
+// let medeelel = () => {
+//     console.log(email.value);
+//     console.log(password.value);
+//     console.log(repeat.value);
     
-}
+// }
 let checked =()=> {
     
         if(password.value === repeat.value&& password.value.length>8&&email.value.indexof('@')!=-1&&email.value.indexof('.')!=-1){
             return true;
         }
         else return false;   
-const utga = document.getElementsByTagName("input").length;
 
+
+}
+let inputs = document.getElementsByTagName("input");
+const utga = document.getElementsByTagName("input").length;
 for (let i = 0; i < utga; i++) {
   // input бүрт addEventListener өгч change эвэнтийг сонсож байна
   inputs[i].addEventListener("change", () => {
@@ -32,6 +35,23 @@ for (let i = 0; i < utga; i++) {
     console.log("Email-ийн утга: ", inputs.email.value);
     console.log("Password-ийн утга: ", inputs.password.value);
     console.log("repeatPassword-ийн утга: ", inputs.repeat.value);
+    if(inputs.password.value!=""&&inputs.repeat.value!=""){
+      if(inputs.password.value == inputs.repeat.value){
+        inputs.password.classList.add("success");
+        inputs.repeat.classList.add("success");
+        inputs.password.classList.remove("error");
+        inputs.repeat.classList.remove("error");
+      }
+      else {
+        
+        inputs.password.classList.add("error");
+        inputs.repeat.classList.add("error");
+        inputs.password.classList.remove("success");
+        inputs.repeat.classList.remove("success");
+      }
+
+    }
+    
 
     // бүх input үүдийг авч зөв эсэхийг шалгах фүнкц дуудаж байна
     if (checked(inputs)) {
@@ -39,9 +59,7 @@ for (let i = 0; i < utga; i++) {
     }
   });
 }
-     
-}
 
 
-button.addEventListener("click",medeelel);
+// button.addEventListener("click",medeelel);
 button.addEventListener("click",checked);
--- js_lessons/event/js/event.js
+++ js_lessons/event/js/event.js
@@ -9,9 +9,9 @@ const cardItem = (utga) => {    /// add hiisen ved jagsaaltan door input bolon b
         <input type="text" id="text" readonly value="${utga}">
     
 
-     <button class="edit" > <i class="fa-sharp fa-solid fa-pen"></i></button>
-    <button class="done"> <i class="fa-solid fa-check" style="color: green;"></i></button>
-    <button class="delete" onclick="ustgah(this)">
+     <button class="edit" onclick="edit(this)" > <i class="fa-sharp fa-solid fa-pen"></i></button>
+     <button class="done" onclick="checked(this)"> <i class="fa-solid fa-check" style="color: green;"></i></button>
+     <button class="delete" onclick="ustgah(this)">
          <i class="fa-sharp fa-solid fa-trash " style="color: red;"></i></button> </div>`;
     return item;
 
@@ -34,10 +34,30 @@ const ustgah = (e) => {        //ustgah function
 }
 
 const edit = (e) => {        //edit hiihdee
-    let add = document.getElementById("text") ;
-    const ad=add.value;
-     
 
+    let tasktext = e.parentNode.children[0];     
+    if(tasktext.hasAttribute("readonly")){
+        tasktext.removeAttribute("readonly");
+        e.children[0].classList.remove("fa-pen");
+        e.children[0].classList.add("fa-plus");
+    }
+    else {
+        tasktext.setAttribute("readonly", "readonly");
+        tasktext.setAttribute("value", tasktext.value);
+        e.children[0].classList.add("fa-pen");
+        e.children[0].classList.remove("fa-plus");
+    }
+    // const ad=add.value;
+}
+
+const checked =(e)=>{
+    let check=e.parentNode.children[0];
+    if(check.classList.contains("line")) {
+        check.classList.remove("line");
+    }
+     else {
+        check.classList.add("line");
+     }
 }
 addbtn.addEventListener("click", nemeh); // button darah ved nemeh uilel hiine gesniig zaana
 document.addEventListener('keyup', (e) => {
