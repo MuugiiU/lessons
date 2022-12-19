19f73db73defe722757cc87d423be83516d8e7ac
muugii <muugii@muugiis-MacBook-Pro.local>
2022-12-19 13:04:25 +0800

    new

---
+++ js_lessons/js_methods/js/shopping_cart.js
@@ -0,0 +1,6 @@
+const shoppingCart=['Milk','Coffee','Tea','Honey'];
+console.log(shoppingCart.unshift('Meat'));
+console.log(shoppingCart.unshift('Sugar'));
+console.log(shoppingCart);
+console.log(shoppingCart.pop());
+console.log(shoppingCart.replaceall('Tea','GreenTea'));
