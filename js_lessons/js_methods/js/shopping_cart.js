462ecb10a93e3d5950088c39193ccbee6e765e04
Evening <evening@admins-iMac.local>
2022-12-19 15:04:16 +0800

    b

--- js_lessons/js_methods/js/shopping_cart.js
+++ js_lessons/js_methods/js/shopping_cart.js
@@ -1,6 +1,13 @@
-const shoppingCart=['Milk','Coffee','Tea','Honey'];
+let shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
 console.log(shoppingCart.unshift('Meat'));
 console.log(shoppingCart.unshift('Sugar'));
 console.log(shoppingCart);
 console.log(shoppingCart.pop());
-console.log(shoppingCart.replaceall('Tea','GreenTea'));
+console.log(shoppingCart);
+let count = [];
+for (let i = 0; i <= shoppingCart.length; i++) {
+    if (shoppingCart[i] === 'Tea') {
+        shoppingCart[i] = shoppingCart[i].replaceAll("Tea", "Green Tea");
+ }
+}
+console.log(shoppingCart);
