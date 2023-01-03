1d0c4b63e784ceea38e300ef6f41dda75afae8e2
muugii <muugii@muugiis-MacBook-Pro.local>
2023-01-03 13:20:01 +0800

    n

--- js_lessons/js_function/function_js/tip.js
+++ js_lessons/js_function/function_js/tip.js
@@ -4,8 +4,19 @@
 // 2. Зөвхөн нэмэгдэл төлбөрийг олдог 1 function.
 // 3. Нийт төлбөрийн дундаж - ийг олдог 1 function.
 
+let totalPrice = 0
+let orderNumber = 0
+
 console.log("============n-1===============");
-// let une=parseInt(prompt("unen dungee oruulna uu"));
+let une1=parseInt(prompt("unen dungee oruulna uu"));
+const moneyToPay = paymentWithTips(une1)
+console.log("Toloh dun: ", moneyToPay)
+console.log("Niit dun: ", total(moneyToPay))
+console.log("dundaj: ", findAvarage())
+
+
+
+
 // let sum=[];
 // let total = function() {
 //  
@@ -22,19 +33,34 @@ console.log("============n-1===============");
 
 console.log("==========n-2==============");
 // zuvhun nemegdel tulbur oldog function
-let une=parseInt(prompt("unen dungee oruulna uu"));
-let sum=[];
-let total = function() {
-  
-  if( une<=5000 && une>=30000) {
-     sum=(une*20)/100;
-    console.log(sum);
-  } else {
-    sum=une*15/100;
-    console.log(sum);
-  }
-  return sum;
-}
-total();
+let une2=parseInt(prompt("unen dungee oruulna uu"));
+const moneyToPay2 = paymentWithTips(une2)
+console.log("Toloh dun: ", moneyToPay2)
+console.log("Niit dun: ", total(moneyToPay2))
+console.log("dundaj: ", findAvarage())
+
+// total();
 console.log("==========n-3=============");
-let 
+let une3=parseInt(prompt("unen dungee oruulna uu"));
+const moneyToPay3 = paymentWithTips(une3)
+console.log("Toloh dun: ", moneyToPay3)
+console.log("Niit dun: ", total(moneyToPay3))
+console.log("dundaj: ", findAvarage())
+
+function total(addingPrice) {
+  totalPrice += addingPrice
+  orderNumber += 1
+  return totalPrice
+}
+
+function paymentWithTips(price) {
+  if (!price) return console.log('aldaa ', price)
+
+  if (price > 5000 && price < 30000) return Math.round(price * 1.15)
+
+  return Math.round(price * 1.2)
+}
+
+function findAvarage () {
+  return Math.round(totalPrice / orderNumber)
+}
